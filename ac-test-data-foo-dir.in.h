900479ceedddc6e3867c467bbedb7a5310414041
Borislav Stanimirov <b@ibob.bg>
2024-11-18 15:32:45 +0200

    chore: create test data

---
+++ ac-test-data-foo-dir.in.h
@@ -0,0 +1,5 @@
+// Copyright (c) Alpaca Core
+// SPDX-License-Identifier: MIT
+//
+#pragma once
+#define AC_TEST_DATA_FOO_DIR "@CMAKE_CURRENT_SOURCE_DIR@"
---
+++ ac-test-data-foo-models.h
@@ -0,0 +1,11 @@
+// Copyright (c) Alpaca Core
+// SPDX-License-Identifier: MIT
+//
+#pragma once
+#include "ac-test-data-foo-dir.h"
+
+#define AC_FOO_MODEL_SMALL_ASSET "foo-small.txt"
+#define AC_FOO_MODEL_LARGE_ASSET "foo-large.txt"
+
+#define AC_FOO_MODEL_SMALL AC_TEST_DATA_FOO_DIR "/" AC_FOO_MODEL_SMALL_ASSET
+#define AC_FOO_MODEL_LARGE AC_TEST_DATA_FOO_DIR "/" AC_FOO_MODEL_LARGE_ASSET
